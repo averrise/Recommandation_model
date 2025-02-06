558664a9d13083ed2465a70f3fa57f290657425d
hangyeongmin <ray7869@naver.com>
2025-02-06 17:54:23 +0900

    post method and kafka

--- FastAPI/app/kafka_producer.py
+++ FastAPI/app/kafka_producer.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from confluent_kafka import Producer
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,15 +11,27 @@ producer_config = {
 
 producer = Producer(producer_config)
 
+class Movie(BaseModel):
+    Movie_Name : str
+    Similarity_weight : int
+    top_n : int
 
 @app.post("/send_message/")
-async def send_message(message: str):
+async def send_message(movie : Movie):
     """
     메시지를 Kafka 토픽으로 전송하는 API 엔드포인트
     """
     try:
-        producer.produce("test-topic", message.encode("utf-8"))
-        producer.flush()  # 메시지가 전송될 때까지 기다림
+        # 메시지를 JSON 형식으로 변환
+        message = {
+            "Movie_Name": movie.Movie_Name,
+            "Similarity_weight": movie.Similarity_weight,
+            "top_n": movie.top_n
+        }
+        # Kafka로 메시지 전송
+        producer.produce("test-topic", str(message).encode("utf-8"))
+        producer.flush()  # 메시지가 전송될 때까지 대기
+
         return {"status": "Message sent", "message": message}
     except Exception as e:
         return {"status": "Error", "error": str(e)}
--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,3 +1,72 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name" : "john",
+        "age" : 17,
+        "class" : "year 12"
+    }
+}
+
+class Student(BaseModel):
+    name : str
+    age : int
+    year : str
+
+class UpdateStudent(BaseModel):
+    name: str | None = None
+    age: str | None = None
+    year: str | None = None
+
+@app.get("/")
+def index():
+    return{"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt = 0, lt = 3)):
+    if student_id not in students:
+        raise HTTPException(status_code=404, detail="Student not found")  # ❌ KeyError 방지
+
+    return students[student_id]
+
+@app.get("/get-by-name/{student_id}")
+def get_student(* , student_id: int ,name : str | None = None, test : int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id : int, student : Student):
+    if student_id in students:
+        return {"Error" : "Student exists"}
+
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id : int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+
+    if student.name is not None:
+        students[student_id].name = student.name
+
+    if student.age is not None:
+        students[student_id].age = student.age
+
+    if student.year is not None:
+        students[student_id].year = student.year
+
+    return students[student_id]
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error" : "Student does not exist"}
+
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}
